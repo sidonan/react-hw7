bc4b7e504a146c70dff81aba16a33efe1026fb81
sidonan <niyozmo2007@gmail.com>
2024-11-14 10:44:42 +0600

    add products list and style for product card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import { ProductCard } from "./components/ProductCard";
 
+const products = [
+  {
+    id: 0,
+    name: "Product 1",
+    price: 100,
+    category: "Category 1",
+    img: "https://object.pscloud.io/cms/cms/Photo/img_0_792_805_2.jpg"
+  },
+  {
+    id: 1,
+    name: "Product 2",
+    price: 100,
+    category: "Category 1",
+    img: "https://object.pscloud.io/cms/cms/Photo/img_0_792_805_2.jpg"
+  },
+]
+
 function App() {
   return (
     <div>
-      <ProductCard/>
+      {
+        products.map((product)=>(
+          <ProductCard data={product} />
+        ))
+      }
     </div>
   );
 }
--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,3 +1,12 @@
-export function ProductCard(){
-    return <div>Product Card</div>
+export function ProductCard(props){
+
+    const {img, name, category, price} = props.data;
+
+    return (<div className="product-card" >
+        <img width={200}  src={img} alt="" />
+        <br />
+        <span>{category}</span>
+        <h4>{name}</h4>
+        <span>{price}</span>
+    </div>);
 }
